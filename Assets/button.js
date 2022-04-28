57df4d47284a0a2554bd919ebb64e9c8ca1bc6ea
Neale Philippe <neale@adflexmarketing.com.au>
2022-04-28 15:33:12 +1000

    updated repo and updated menu for mobile screen

--- Assets/button.js
+++ Assets/button.js
@@ -17,3 +17,11 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
